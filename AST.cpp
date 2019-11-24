2bf80e2a01323b0ddb797b61b3ae5709c0924e57
SubbulakshmiRS <rs_subbulakshmi@hotmail.com>
2019-11-25 04:38:34 +0530

    visitor traverse

--- AST.cpp
+++ AST.cpp
@@ -14,13 +14,26 @@ void AST_stmts::push_back(AST_stmt* stmt)
 {
     this->stmt_list.push_back(stmt);
 }
-
+void AST_stmts::accept(Visitor & v)
+{
+    v.visit(this);
+}
 //stmt
+
+//semicolon
+void AST_semicolon::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //keyword
 AST_keyword::AST_keyword(string keyword_type)
 {
     this->keyword_type = keyword_type;
+}
+void AST_keyword::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //function_decl
@@ -31,12 +44,20 @@ AST_function_decl::AST_function_decl(string dtype, string functionName, AST_para
     this->paramD_list = paramD_list;
     this->stmts = stmts;
 }
+void AST_function_decl::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //variable_decl
 AST_variable_decl::AST_variable_decl(string dtype, AST_variable* variable)
 {
     this->dtype = dtype;
     this->variable = variable;
+}
+void AST_variable_decl::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //assignStmt
@@ -47,6 +68,10 @@ AST_assignStmt_old::AST_assignStmt_old(AST_variable* varName, AST_expr* expr)
     this->varName = varName;
     this->expr = expr;
 }
+void AST_assignStmt_old::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //AST_assignStmt_new
 AST_assignStmt_new::AST_assignStmt_new(string dtype, AST_variable* varName, AST_expr* expr)
@@ -54,6 +79,10 @@ AST_assignStmt_new::AST_assignStmt_new(string dtype, AST_variable* varName, AST_
     this->dtype = dtype;
     this->varName = varName;
     this->expr = expr;
+}
+void AST_assignStmt_new::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //functionCall 
@@ -63,12 +92,20 @@ AST_functionCall_noargs::AST_functionCall_noargs(string functionName)
 {
     this->functionName = functionName;
 }
+void AST_functionCall_noargs::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //functionCall_param
 AST_functionCall_args::AST_functionCall_args(string functionName, AST_param_list * param_list)
 {
     this->functionName = functionName;
     this->param_list = param_list;
+}
+void AST_functionCall_args::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //ifStmt
@@ -79,6 +116,10 @@ AST_ifWEStmt::AST_ifWEStmt(AST_expr* expr, AST_stmts* ifStmts)
     this->expr = expr;
     this->ifStmts = ifStmts;
 }
+void AST_ifWEStmt::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //ifEStmt
 AST_ifElseStmt::AST_ifElseStmt(AST_expr * expr, AST_stmts* ifStmts, AST_stmts* ifEStmts)
@@ -87,12 +128,20 @@ AST_ifElseStmt::AST_ifElseStmt(AST_expr * expr, AST_stmts* ifStmts, AST_stmts* i
     this->ifStmts = ifStmts;
     this->ifEStmts = ifEStmts;
 }
+void AST_ifElseStmt::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //whileStmt
 AST_whileStmt::AST_whileStmt(AST_expr* expr, AST_stmts* whileStmts)
 {
     this->expr = expr;
     this->whileStmts = whileStmts;
+}
+void AST_whileStmt::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //forStmt
@@ -104,11 +153,19 @@ AST_forStmt::AST_forStmt(AST_variable* intialize_var, AST_variable* start, AST_v
     this->end = end;
     this->forStmts = forStmts;
 }
+void AST_forStmt::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //returnStmt
 AST_returnStmt::AST_returnStmt(AST_expr* expr)
 {
     this->expr = expr;
+}
+void AST_returnStmt::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //inputStmt
@@ -116,11 +173,19 @@ AST_inputStmt::AST_inputStmt(AST_variable* var)
 {
     this->var = var;
 }
+void AST_inputStmt::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //outputStmt
 AST_outputStmt::AST_outputStmt(AST_variable* var)
 {
     this->var = var;
+}
+void AST_outputStmt::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //expr
@@ -131,6 +196,10 @@ AST_expr_unary::AST_expr_unary(string opType, AST_expr* expr)
     this->opType = opType;
     this->expr = expr;
 }
+void AST_expr_unary::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //expr_binary
 AST_expr_binary::AST_expr_binary(AST_expr* expr1, string opType, AST_expr* expr2)
@@ -138,6 +207,10 @@ AST_expr_binary::AST_expr_binary(AST_expr* expr1, string opType, AST_expr* expr2
     this->expr1 = expr1;
     this->opType = opType;
     this->expr2 = expr2;
+}
+void AST_expr_binary::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //expr_ternary
@@ -147,11 +220,19 @@ AST_expr_ternary::AST_expr_ternary(AST_expr* expr1, AST_expr* expr2, AST_expr* e
     this->expr2 = expr2;
     this->expr3 = expr3;
 }
+void AST_expr_ternary::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //paramD_list
 void AST_paramD_list::push_back(AST_paramD* paramD)
 {
     this->paramDs.push_back(paramD);
+}
+void AST_paramD_list::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //param_list
@@ -159,12 +240,20 @@ void AST_param_list::push_back(AST_param* param)
 {
     this->params.push_back(param);
 }
+void AST_param_list::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //paramD
 AST_paramD::AST_paramD(string dtype, AST_variable* var)
 {
     this->dtype = dtype;
     this->var =  var;
+}
+void AST_paramD::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //param
@@ -174,17 +263,29 @@ AST_int::AST_int(int value)
 {
     this->value = value;
 }
+void AST_int::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //bool
 AST_bool::AST_bool(int value)
 {
     this->value = value;
 }
+void AST_bool::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //float
 AST_float::AST_float(float value)
 {
     this->value = value;
+}
+void AST_float::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //variable
@@ -194,6 +295,10 @@ AST_variable_0D::AST_variable_0D(string variableName)
 {
     this->variableName = variableName;
 }
+void AST_variable_0D::accept(Visitor & v)
+{
+    v.visit(this);
+}
 
 //variable_1D
 AST_variable_1D_v::AST_variable_1D_v(string variableName, AST_variable_0D* index)
@@ -201,10 +306,19 @@ AST_variable_1D_v::AST_variable_1D_v(string variableName, AST_variable_0D* index
     this->variableName = variableName;
     this->index = index;
 }
+void AST_variable_1D_v::accept(Visitor & v)
+{
+    v.visit(this);
+}
+
 AST_variable_1D_i::AST_variable_1D_i(string variableName, int index)
 {
     this->variableName = variableName;
     this->index = index;
+}
+void AST_variable_1D_i::accept(Visitor & v)
+{
+    v.visit(this);
 }
 
 //variable_2D
@@ -214,21 +328,275 @@ AST_variable_2D_ii::AST_variable_2D_ii(string variableName, int index1,int index
     this->index1 = index1;
     this->index2 = index2;
 }
+void AST_variable_2D_ii::accept(Visitor & v)
+{
+    v.visit(this);
+}
+
 AST_variable_2D_iv::AST_variable_2D_iv(string variableName, int index1,AST_variable_0D* index2)
 {
     this->variableName = variableName;
     this->index1 = index1;
     this->index2 = index2;
 }
+void AST_variable_2D_iv::accept(Visitor & v)
+{
+    v.visit(this);
+}
+
 AST_variable_2D_vi::AST_variable_2D_vi(string variableName, AST_variable_0D* index1,int index2)
 {
     this->variableName = variableName;
     this->index1 = index1;
     this->index2 = index2;
 }
+void AST_variable_2D_vi::accept(Visitor & v)
+{
+    v.visit(this);
+}
+
 AST_variable_2D_vv::AST_variable_2D_vv(string variableName, AST_variable_0D* index1,AST_variable_0D* index2)
 {
     this->variableName = variableName;
     this->index1 = index1;
     this->index2 = index2;
+}
+void AST_variable_2D_vv::accept(Visitor & v)
+{
+    v.visit(this);
+}
+
+
+void Traverse::visit(AST_stmts * stmts)
+{
+    cout << "stmts" << endl;
+    for(int i=0;i<(int)stmts->stmt_list.size();i++)
+        stmts->stmt_list[i]->accept(*this);
+}
+
+//funtion_decl
+void Traverse::visit(AST_function_decl * function_decl)
+{
+    cout<<"funtion decl"<<endl;
+    cout<<"dtype "<<function_decl->dtype<<endl;
+    cout<<"functionName "<<function_decl->functionName<<endl;
+    function_decl->paramD_list->accept(*this);
+    function_decl->stmts->accept(*this);
+}
+
+//variable_decl
+void Traverse::visit(AST_variable_decl *variable_decl)
+{
+    cout<<"variable decl"<<endl;
+    cout<<"dtype "<<variable_decl->dtype<<endl;
+    variable_decl->variable->accept(*this);
+}
+
+void Traverse::visit(AST_semicolon* semicolon)
+{
+    cout<<"empty statement "<<endl;
+}
+
+void Traverse::visit(AST_keyword* keyword)
+{
+    cout<<"keyword statement "<<keyword->keyword_type<<endl;
+}
+
+//assign_stmt_old
+void Traverse::visit(AST_assignStmt_old *assignStmt_old)
+{
+    cout<<"assignment of old variable"<<endl;
+    assignStmt_old->varName->accept(*this);
+    assignStmt_old->expr->accept(*this);
+}
+
+//assign_stmt_new
+void Traverse::visit(AST_assignStmt_new *assignStmt_new)
+{
+    cout<<"assignment of new variable"<<endl;
+    cout<<"dtype "<<assignStmt_new->dtype<<endl;
+    assignStmt_new->varName->accept(*this);
+    assignStmt_new->expr->accept(*this);
+}
+
+//functionCall_noargs
+void Traverse::visit(AST_functionCall_noargs *functionCall_noargs)
+{
+    cout<<"functionCall with no args"<<endl;
+    cout<<"functionName "<<functionCall_noargs->functionName<<endl;
+}
+
+//functionCall_args
+void Traverse::visit(AST_functionCall_args *functionCall_args)
+{
+    cout<<"functionCall with args"<<endl;
+    cout<<"functionName "<<functionCall_args->functionName<<endl;
+    functionCall_args->param_list->accept(*this);
+}
+
+//ifWEStmt
+void Traverse::visit(AST_ifWEStmt *ifWEStmt)
+{
+    cout<<"if stmt"<<endl;
+    ifWEStmt->expr->accept(*this);
+    ifWEStmt->ifStmts->accept(*this);
+}
+
+//ifElseStmt
+void Traverse::visit(AST_ifElseStmt *ifElseStmt)
+{   
+    cout<<"if else stmt"<<endl;
+    ifElseStmt->expr->accept(*this);
+    ifElseStmt->ifStmts->accept(*this);
+    ifElseStmt->ifEStmts->accept(*this);
+}
+
+void Traverse::visit(AST_whileStmt *whileStmt)
+{
+    cout<<"while stmt"<<endl;
+    cout<<"while condition"<<endl;
+    whileStmt->expr->accept(*this);
+    cout<<"while stmts"<<endl;
+    whileStmt->whileStmts->accept(*this);
+}
+
+void Traverse::visit(AST_forStmt *forStmt)
+{
+    cout<<"for stmt"<<endl;
+    cout<<"for initialize"<<endl;
+    forStmt->initialize->accept(*this);
+    cout<<"for condition"<<endl;
+    forStmt->expr->accept(*this);
+    cout<<"for increment"<<endl;
+    forStmt->increment->accept(*this);
+    cout<<"for stmts"<<endl;
+    forStmt->forStmts->accept(*this);
+}
+
+void Traverse::visit(AST_returnStmt *returnStmt)
+{
+    cout<<"return stmt"<<endl;
+    returnStmt->expr->accept(*this);
+}
+
+void Traverse::visit(AST_inputStmt *inputStmt)
+{
+    cout<<"read input stmt"<<endl;
+    inputStmt->var->accept(*this);
+}
+
+void Traverse::visit(AST_outputStmt *outputStmt)
+{
+    cout<<"write output stmt"<<endl;
+    outputStmt->var->accept(*this);
+}
+
+void Traverse::visit(AST_expr_unary *expr_unary)
+{
+    cout<<"unary expression"<<endl;
+    cout<<"operator "<<expr_unary->opType<<endl;
+    expr_unary->expr->accept(*this);
+
+}
+
+void Traverse::visit(AST_expr_binary *expr_binary)
+{
+    cout<<"binary expression"<<endl;
+    expr_binary->expr1->accept(*this);
+    cout<<"operator "<<expr_binary->opType<<endl;
+    expr_binary->expr2->accept(*this);
+}
+
+void Traverse::visit(AST_expr_ternary *expr_ternary)
+{
+    cout<<"ternary expression"<<endl;
+    expr_ternary->expr1->accept(*this);
+    cout<<"?"<<endl;
+    expr_ternary->expr2->accept(*this);
+    cout<<":"<<endl;
+    expr_ternary->expr3->accept(*this);
+}
+
+void Traverse::visit(AST_paramD_list *paramD_list)
+{
+    cout<<"param list of declarations"<<endl;
+    for(int i=0;i<(int)paramD_list->paramDs.size();i++)
+        paramD_list->paramDs[i]->accept(*this);
+
+}
+
+void Traverse::visit(AST_param_list *param_list)
+{
+    cout<<"param list of variables"<<endl;
+    for(int i=0;i<(int)param_list->params.size();i++)
+        param_list->params[i]->accept(*this);
+
+}
+
+void Traverse::visit(AST_paramD *paramD)
+{
+    cout<<"param with declaration"<<endl;
+    cout<<"dtype "<<paramD->dtype<<endl;
+    paramD->param->accept(*this);
+}
+
+void Traverse::visit(AST_int *intVal)
+{
+    cout<<"int literal "<<intVal->value<<endl;
+}
+
+void Traverse::visit(AST_float *floatVal)
+{
+    cout<<"float literal "<<floatVal->value<<endl; 
+}
+
+void Traverse::visit(AST_bool* boolVal)
+{
+    cout<<"bool literal "<<boolVal->value<<endl;
+}
+
+void Traverse::visit(AST_variable_0D *variable_0D)
+{
+    cout<<"0D variable "<<variable_0D->variableName<<endl;
+}
+
+void Traverse::visit(AST_variable_1D_v *variable_1D_v)
+{
+    cout<<"1D variable "<<variable_1D_v->variableName<<endl;
+    cout<<"index"<<endl;
+    variable_1D_v->index->accept(*this);
+}
+
+void Traverse::visit(AST_variable_1D_i *variable_1D_i)
+{
+    cout<<"1D variable "<<variable_1D_i->variableName<<" index "<<variable_1D_i->index<<endl;
+}
+
+void Traverse::visit(AST_variable_2D_ii *variable_2D_ii)
+{
+    cout<<"2D variable "<<variable_2D_ii->variableName<<" index1 "<<variable_2D_ii->index1<<" index2 "<<variable_2D_ii->index2<<endl;
+}
+
+void Traverse::visit(AST_variable_2D_iv *variable_2D_iv)
+{
+    cout<<"2D variable "<<variable_2D_iv->variableName<<" index1 "<<variable_2D_iv->index1<<endl;
+    cout<<"index2"<<endl;
+    variable_2D_iv->index2->accept(*this); 
+}
+
+void Traverse::visit(AST_variable_2D_vi *variable_2D_vi)
+{
+    cout<<"2D variable "<<variable_2D_vi->variableName<<endl;
+    cout<<"index1"<<endl;
+    variable_2D_vi->index1->accept(*this);
+    cout<<"index2 "<<variable_2D_vi->index2<<endl;
+}
+
+void Traverse::visit(AST_variable_2D_vv *variable_2D_vv)
+{
+    cout<<"2D variable "<<variable_2D_vv->variableName<<endl;
+    cout<<"index1"<<endl;
+    variable_2D_vv->index1->accept(*this);
+    cout<<"index2"<<endl;
+    variable_2D_vv->index2->accept(*this);
 }
--- AST.h
+++ AST.h
@@ -104,9 +104,53 @@ union _NODE_
 typedef union _NODE_ YYSTYPE;
 #define YYSTYPE_IS_DECLARED 1
 
+class Visitor
+{
+public:
+    virtual void visit(AST_node *) = 0;
+    virtual void visit(AST_stmts *) = 0;
+
+    virtual void visit(AST_function_decl *) = 0;
+    virtual void visit(AST_variable_decl *) = 0;
+    virtual void visit(AST_semicolon* ) = 0;
+    virtual void visit(AST_keyword* ) = 0;
+    virtual void visit(AST_assignStmt_old *) = 0;
+    virtual void visit(AST_assignStmt_new *) = 0;
+    virtual void visit(AST_ifStmt *) = 0;
+    virtual void visit(AST_ifWEStmt *) = 0;
+    virtual void visit(AST_ifElseStmt *) = 0;
+    virtual void visit(AST_whileStmt *) = 0;
+    virtual void visit(AST_forStmt *) = 0;
+
+    virtual void visit(AST_returnStmt *) = 0;
+    virtual void visit(AST_inputStmt *) = 0;
+    virtual void visit(AST_outputStmt *) = 0;
+    virtual void visit(AST_expr_unary *) = 0;
+    virtual void visit(AST_expr_binary *) = 0;
+    virtual void visit(AST_expr_ternary *) = 0;    
+    virtual void visit(AST_functionCall_noargs *) = 0;
+    virtual void visit(AST_functionCall_args *) = 0;
+
+    virtual void visit(AST_paramD_list *) = 0;
+    virtual void visit(AST_param_list *) = 0;
+    virtual void visit(AST_paramD *) = 0;
+
+    virtual void visit(AST_int *) = 0;
+    virtual void visit(AST_float *) = 0;
+    virtual void visit(AST_bool *) = 0;
+    virtual void visit(AST_variable_0D *) = 0;
+    virtual void visit(AST_variable_1D_v *) = 0;
+    virtual void visit(AST_variable_1D_i*) = 0;
+    virtual void visit(AST_variable_2D_ii *) = 0;
+    virtual void visit(AST_variable_2D_iv *) = 0;
+    virtual void visit(AST_variable_2D_vi *) = 0;
+    virtual void visit(AST_variable_2D_vv *) = 0;
+};
+
 class AST_node
 {
-
+public:
+    virtual void accept(Visitor &) = 0;
 };
 
 class AST_prog : public AST_node
@@ -118,9 +162,11 @@ class AST_prog : public AST_node
 class AST_stmts : public AST_prog
 {
 private:
+    friend class Traverse;
     vector<AST_stmt*> stmt_list;
 public:
     void push_back(AST_stmt * stmt);
+    void accept(Visitor &);
 };
 
 class AST_stmt : public AST_node
@@ -129,21 +175,27 @@ class AST_stmt : public AST_node
 };
 
 class AST_semicolon : public AST_stmt{
-
+private:
+    friend class Traverse;
+private:
+    void accept(Visitor &);
 };
 
 class AST_keyword : public AST_stmt
 {
 private:
+    friend class Traverse;
     string keyword_type; 
 public:
     AST_keyword(string keyword_type);
+    void accept(Visitor &);
 
 };
 
 class AST_function_decl: public AST_stmt
 {
 private:
+    friend class Traverse;
     string dtype;
     string functionName;
     AST_paramD_list* paramD_list;
@@ -151,16 +203,19 @@ private:
 
 public:
     AST_function_decl(string dtype, string functionName, AST_paramD_list* paramD_list, AST_stmts* stmts);
+    void accept(Visitor &);
 };
 
 class AST_variable_decl: public AST_stmt
 {
 private:
+    friend class Traverse;
     string dtype;
     AST_variable* variable;  
 
 public:
     AST_variable_decl(string dtype, AST_variable* variable);
+    void accept(Visitor &);
 };
 
 class AST_assignStmt : public AST_stmt
@@ -171,21 +226,25 @@ class AST_assignStmt : public AST_stmt
 class AST_assignStmt_old: public AST_assignStmt
 {
 private:
+    friend class Traverse;
     AST_variable* varName;
     AST_expr* expr;
 public:
     AST_assignStmt_old(AST_variable* varName, AST_expr* expr);
+    void accept(Visitor &);
 
 };
 
 class AST_assignStmt_new: public AST_assignStmt
 {
 private:
+    friend class Traverse;
     string dtype;
     AST_variable* varName;
     AST_expr* expr;
 public:
     AST_assignStmt_new(string dtype, AST_variable* varName, AST_expr* expr);
+    void accept(Visitor &);
 
 };
 
@@ -197,34 +256,41 @@ class AST_ifStmt : public AST_stmt
 class AST_ifWEStmt : public AST_ifStmt
 {
 private:
+    friend class Traverse;
     AST_expr* expr;
     AST_stmts* ifStmts;
 public:
     AST_ifWEStmt(AST_expr* expr, AST_stmts* ifStmts);
+    void accept(Visitor &);
 };
 
 class AST_ifElseStmt : public AST_ifStmt
 {
 private:
+    friend class Traverse;
     AST_expr* expr;
     AST_stmts* ifStmts;
     AST_stmts* ifEStmts;
 public:
     AST_ifElseStmt(AST_expr * expr, AST_stmts* ifStmts, AST_stmts* ifEStmts);
+    void accept(Visitor &);
 };
 
 class AST_whileStmt : public AST_stmt
 {
 private:
+    friend class Traverse;
     AST_expr* expr;
     AST_stmts* whileStmts;
 public:
     AST_whileStmt(AST_expr* expr, AST_stmts* whileStmts);
+    void accept(Visitor &);
 };
 
 class AST_forStmt : public AST_stmt
 {
 private:
+    friend class Traverse;
     AST_variable* intialize_var;
     AST_variable*  start;
     AST_variable*  step;
@@ -232,30 +298,37 @@ private:
     AST_stmts* forStmts;
 public:
     AST_forStmt(AST_variable* intialize_var, AST_variable*  start, AST_variable*  step, AST_variable*  end, AST_stmts* forStmts);
+    void accept(Visitor &);
 };
 
 class AST_returnStmt : public AST_stmt
 {
 private:
+    friend class Traverse;
     AST_expr* expr;
 public:
     AST_returnStmt(AST_expr* expr);
+    void accept(Visitor &);
 };
 
 class AST_inputStmt : public AST_stmt
 {
 private:
+    friend class Traverse;
     AST_variable* var;
 public:
     AST_inputStmt(AST_variable* var);
+    void accept(Visitor &);
 };
 
 class AST_outputStmt : public AST_stmt
 {
 private:
+    friend class Traverse;
     AST_variable* var;
 public:
     AST_outputStmt(AST_variable* var);
+    void accept(Visitor &);
 };
 
 class AST_expr: public AST_node
@@ -266,27 +339,33 @@ class AST_expr: public AST_node
 class AST_expr_unary: public AST_expr
 {
 private:
+    friend class Traverse;
     string opType;
     AST_expr* expr;
 public:
     AST_expr_unary(string opType, AST_expr* expr);
+    void accept(Visitor &);
 };
 
 class AST_expr_binary: public AST_expr
 {
 private:
+    friend class Traverse;
     string opType;
     AST_expr* expr1, *expr2;
 public:
     AST_expr_binary(AST_expr* expr1, string opType, AST_expr* expr2);
+    void accept(Visitor &);
 };
 
 class AST_expr_ternary: public AST_expr
 {
 private:
+    friend class Traverse;
     AST_expr* expr1, *expr2, *expr3;
 public:
     AST_expr_ternary(AST_expr* expr1, AST_expr* expr2, AST_expr* expr3);
+    void accept(Visitor &);
 };
 
 class AST_functionCall : public AST_expr
@@ -297,44 +376,58 @@ class AST_functionCall : public AST_expr
 class AST_functionCall_noargs : public AST_functionCall
 {
 private:
+    friend class Traverse;
     string functionName;
 public:
     AST_functionCall_noargs(string functionName);
+    void accept(Visitor &);
 };
 
 class AST_functionCall_args : public AST_functionCall
 {
 private:
+    friend class Traverse;
     string functionName;
     AST_param_list * param_list;
 public:
     AST_functionCall_args(string functionName, AST_param_list * param_list);
+    void accept(Visitor &);
 };
 
 class AST_paramD_list : public AST_node
 {
 private:
+    friend class Traverse;
     vector<AST_paramD*> paramDs;
 public:
     void push_back(AST_paramD* paramD);
+    void accept(Visitor &);
 };
 
 class AST_param_list : public AST_node
 {
 private:
+    friend class Traverse;
     vector<AST_param*> params;
 public:
     void push_back(AST_param* param);
+    void accept(Visitor &);
 };
 
 
 class AST_paramD : public AST_node
 {
 private:
+    friend class Traverse;
     string dtype;
     AST_variable* var;
 public:
+<<<<<<< 058108a30e9710e03b59a863b1f6b4d036d43103
     AST_paramD(string dtype, AST_variable* var);
+=======
+    AST_paramD(string dtype, AST_param* param);
+    void accept(Visitor &);
+>>>>>>> visitor traverse
 };
 
 class AST_param : public AST_expr
@@ -345,25 +438,31 @@ class AST_param : public AST_expr
 class AST_int : public AST_param 
 {
 private:
+    friend class Traverse;
     int value;
 public:
     AST_int(int value);
+    void accept(Visitor &);
 };
 
 class AST_bool : public AST_param 
 {
 private:
+    friend class Traverse;
     int value;
 public:
     AST_bool(int value);
+    void accept(Visitor &);
 };
 
 class AST_float : public AST_param 
 {
 private:
+    friend class Traverse;
     float value;
 public:
     AST_float(float value);
+    void accept(Visitor &);
 };
 
 class AST_variable : public AST_param
@@ -374,63 +473,123 @@ class AST_variable : public AST_param
 class AST_variable_0D : public AST_variable 
 {
 private:
+    friend class Traverse;
     string variableName;
 public:
     AST_variable_0D(string variableName);
+    void accept(Visitor &);
 };
 
 class AST_variable_1D_v : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     AST_variable_0D* index;
 public:
     AST_variable_1D_v(string variableName, AST_variable_0D* index);
+    void accept(Visitor &);
 };
 
 class AST_variable_1D_i : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     int index;
 public:
     AST_variable_1D_i(string variableName, int index);
+    void accept(Visitor &);
 };
 
 class AST_variable_2D_ii : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     int index1,index2;
 public:
     AST_variable_2D_ii(string variableName, int index1,int index2);
+    void accept(Visitor &);
 };
 
 class AST_variable_2D_iv : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     int index1;
     AST_variable_0D* index2;
 public:
     AST_variable_2D_iv(string variableName, int index1,AST_variable_0D* index2);
+    void accept(Visitor &);
 };
 
 class AST_variable_2D_vi : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     AST_variable_0D* index1;
     int index2;
 public:
     AST_variable_2D_vi(string variableName, AST_variable_0D* index1, int index2);
+    void accept(Visitor &);
 };
 
 class AST_variable_2D_vv : public AST_variable
 {
 private:
+    friend class Traverse;
     string variableName;
     AST_variable_0D* index1,*index2;
 public:
     AST_variable_2D_vv(string variableName, AST_variable_0D* index1,AST_variable_0D* index2);
+    void accept(Visitor &);
+};
+
+
+class Traverse;
+
+class Traverse : public Visitor
+{
+public:
+     void visit(AST_stmts * stmts) ;
+
+     void visit(AST_function_decl *function_decl) ;
+     void visit(AST_variable_decl *variable_decl) ;
+     void visit(AST_semicolon* semicolon);
+     void visit(AST_keyword* keyword);
+     void visit(AST_assignStmt_old *assignStmt_old) ;
+     void visit(AST_assignStmt_new *assignStmt_new) ;
+     void visit(AST_functionCall_noargs *functionCall_noargs) ;
+     void visit(AST_functionCall_args *functionCall_args) ;
+
+     void visit(AST_ifWEStmt *ifWEStmt) ;
+     void visit(AST_ifElseStmt *ifElseStmt) ;
+     void visit(AST_whileStmt *whileStmt) ;
+     void visit(AST_forStmt *forStmt) ;
+
+     void visit(AST_returnStmt *returnStmt) ;
+     void visit(AST_inputStmt *inputStmt) ;
+     void visit(AST_outputStmt *outputStmt) ;
+     void visit(AST_expr_unary *expr_unary) ;
+     void visit(AST_expr_binary *expr_binary) ;
+     void visit(AST_expr_ternary *expr_ternary) ;    
+
+
+     void visit(AST_paramD_list *paramD_list ) ;
+     void visit(AST_param_list *param_list) ;
+     void visit(AST_paramD *paramD ) ;
+
+     void visit(AST_int *intVal) ;
+     void visit(AST_float *floatVal) ;
+     void visit(AST_bool* boolVal) ;
+     void visit(AST_variable_0D *variable_0D) ;
+     void visit(AST_variable_1D_v *variable_1D_v) ;
+     void visit(AST_variable_1D_i *variable_1D_i) ;
+     void visit(AST_variable_2D_ii *variable_2D_ii) ;
+     void visit(AST_variable_2D_iv *variable_2D_iv) ;
+     void visit(AST_variable_2D_vi *variable_2D_vi) ;
+     void visit(AST_variable_2D_vv *variable_2D_vv) ;
 };
